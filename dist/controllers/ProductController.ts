8efc0216cb63bbba2d3178bae53f378c9e96585e
Duc-tuan <phamductuan26032002@gmail.com>
2023-09-05 10:14:04 +0700

    update

--- dist/controllers/ProductController.ts
+++ dist/controllers/ProductController.ts
@@ -5,90 +5,92 @@ const products = require('../models/ProductModel');
 
 class ProductController {
   //[GET] danh sách sản phẩm tất cả hoặc theo phân trang page&pageSize hoặc tìm kiếm theo query&status
-  index(req: Request, res: Response, next: any) {
-    // var { page, pageSize, query, status } = req.query;
-    // let dataSearch: any = undefined;
-    // let queryData: any = undefined;
+  // index(req: Request, res: Response, next: any) {
+  //   var { page, pageSize, query, status } = req.query;
+  //   let dataSearch: any = undefined;
+  //   let queryData: any = undefined;
 
-    // // query data search
-    // if (status && query) {
-    //   dataSearch = { $regex: query, $options: 'i' };
-    //   queryData = {
-    //     productStatus: status,
-    //     $or: [
-    //       { productName: dataSearch },
-    //       { productPrice: typeof Number(query) === typeof 1 && !Number.isNaN(Number(query)) ? query : null },
-    //       { productQty: typeof Number(query) === typeof 1 && !Number.isNaN(Number(query)) ? query : null },
-    //     ],
-    //   };
-    // } else if (query) {
-    //   dataSearch = { $regex: query, $options: 'im' };
-    //   queryData = {
-    //     $or: [
-    //       { productName: dataSearch },
-    //       { productPrice: typeof Number(query) === typeof 1 && !Number.isNaN(Number(query)) ? query : null },
-    //       { productQty: typeof Number(query) === typeof 1 && !Number.isNaN(Number(query)) ? query : null },
-    //     ],
-    //   };
-    // } else if (status) {
-    //   queryData = { productStatus: status };
-    // }
+  //   // query data search
+  //   if (status && query) {
+  //     dataSearch = { $regex: query, $options: 'i' };
+  //     queryData = {
+  //       productStatus: status,
+  //       $or: [
+  //         { productName: dataSearch },
+  //         { productPrice: typeof Number(query) === typeof 1 && !Number.isNaN(Number(query)) ? query : null },
+  //         { productQty: typeof Number(query) === typeof 1 && !Number.isNaN(Number(query)) ? query : null },
+  //       ],
+  //     };
+  //   } else if (query) {
+  //     dataSearch = { $regex: query, $options: 'im' };
+  //     queryData = {
+  //       $or: [
+  //         { productName: dataSearch },
+  //         { productPrice: typeof Number(query) === typeof 1 && !Number.isNaN(Number(query)) ? query : null },
+  //         { productQty: typeof Number(query) === typeof 1 && !Number.isNaN(Number(query)) ? query : null },
+  //       ],
+  //     };
+  //   } else if (status) {
+  //     queryData = { productStatus: status };
+  //   }
 
-    // var skipNumber: number = 0;
-    // // get page, pageSize, query and status data
-    // if (page || pageSize) {
-    //   const pageSizeNew = Number(pageSize);
-    //   let pageNew = Number(page);
-    //   if (pageNew <= 1) pageNew = 1;
-    //   skipNumber = (pageNew - 1) * pageSizeNew;
+  //   var skipNumber: number = 0;
+  //   // get page, pageSize, query and status data
+  //   if (page || pageSize) {
+  //     const pageSizeNew = Number(pageSize);
+  //     let pageNew = Number(page);
+  //     if (pageNew <= 1) pageNew = 1;
+  //     skipNumber = (pageNew - 1) * pageSizeNew;
 
-    //   products
-    //     .find(queryData, {
-    //       productImageDetail: 0,
-    //       productImageMulter: 0,
-    //       productComment: 0,
-    //       productDescribes: 0,
-    //       productDesc: 0,
-    //     })
-    //     .skip(skipNumber)
-    //     .sort({ index: -1 })
-    //     .limit(pageSize)
-    //     .then((data: any) => {
-    //       products
-    //         .countDocuments(queryData)
-    //         .then((total: number) => {
-    //           var totalPage: number = Math.ceil(total / pageSizeNew);
-    //           return res.status(200).json({
-    //             paganition: {
-    //               totalPage: Number(totalPage),
-    //               currentPage: Number(page),
-    //               pageSize: Number(pageSize),
-    //               totalElement: Number(total),
-    //             },
-    //             data,
-    //           });
-    //         })
-    //         .catch((error: any) => next(error));
-    //     });
-    // } else {
-    //   // get all
-    //   products
-    //     .find(queryData, {
-    //       productImageDetail: 0,
-    //       productImageMulter: 0,
-    //       productComment: 0,
-    //       productDescribes: 0,
-    //       productDesc: 0,
-    //     })
-    //     .sort({ index: -1 })
-    //     .then((data: any) => {
-    //       return res.status(200).json(data);
-    //     })
-    //     .catch((err: any) => {
-    //       return next(err);
-    //     });
-    // }
-    return res.send('SERVER ON');
+  //     products
+  //       .find(queryData, {
+  //         productImageDetail: 0,
+  //         productImageMulter: 0,
+  //         productComment: 0,
+  //         productDescribes: 0,
+  //         productDesc: 0,
+  //       })
+  //       .skip(skipNumber)
+  //       .sort({ index: -1 })
+  //       .limit(pageSize)
+  //       .then((data: any) => {
+  //         products
+  //           .countDocuments(queryData)
+  //           .then((total: number) => {
+  //             var totalPage: number = Math.ceil(total / pageSizeNew);
+  //             return res.status(200).json({
+  //               paganition: {
+  //                 totalPage: Number(totalPage),
+  //                 currentPage: Number(page),
+  //                 pageSize: Number(pageSize),
+  //                 totalElement: Number(total),
+  //               },
+  //               data,
+  //             });
+  //           })
+  //           .catch((error: any) => next(error));
+  //       });
+  //   } else {
+  //     // get all
+  //     products
+  //       .find(queryData, {
+  //         productImageDetail: 0,
+  //         productImageMulter: 0,
+  //         productComment: 0,
+  //         productDescribes: 0,
+  //         productDesc: 0,
+  //       })
+  //       .sort({ index: -1 })
+  //       .then((data: any) => {
+  //         return res.status(200).json(data);
+  //       })
+  //       .catch((err: any) => {
+  //         return next(err);
+  //       });
+  //   }
+  // }
+  index(req: Request, res: Response) {
+    return res.send('home or other');
   }
 
   //[GET] chi tiết sản phẩm
