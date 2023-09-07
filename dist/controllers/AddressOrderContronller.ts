937e67393de4df553ce5c71033d57b56e02cd030
Duc-tuan <phamductuan26032002@gmail.com>
2023-09-07 17:26:14 +0700

    update

--- dist/controllers/AddressOrderContronller.ts
+++ dist/controllers/AddressOrderContronller.ts
@@ -7,15 +7,76 @@ class AddressOrderController {
     //GET /addressOrder
     async index(req: Request, res: Response, next: any) {
         try {
-            AddressModel
-                .find()
-                .sort({ index: -1 })
-                .then((data: any) => {
-                    return res.status(200).json(data);
-                })
-                .catch((err: any) => {
-                    return next(err);
-                });
+            var { page, pageSize, query, status } = req.query;
+            let dataSearch: any = undefined;
+            let queryData: any = undefined;
+
+            // query data search
+            if (status && query) {
+                dataSearch = { $regex: query, $options: 'i' };
+                queryData = {
+                    productStatus: status,
+                    $or: [
+                        { productName: dataSearch },
+                        { productPrice: typeof Number(query) === typeof 1 && !Number.isNaN(Number(query)) ? query : null },
+                        { productQty: typeof Number(query) === typeof 1 && !Number.isNaN(Number(query)) ? query : null },
+                    ],
+                };
+            } else if (query) {
+                dataSearch = { $regex: query, $options: 'im' };
+                queryData = {
+                    $or: [
+                        { productName: dataSearch },
+                        { productPrice: typeof Number(query) === typeof 1 && !Number.isNaN(Number(query)) ? query : null },
+                        { productQty: typeof Number(query) === typeof 1 && !Number.isNaN(Number(query)) ? query : null },
+                    ],
+                };
+            } else if (status) {
+                queryData = { productStatus: status };
+            }
+
+            var skipNumber: number = 0;
+            // get page, pageSize, query and status data
+            if (page || pageSize) {
+                const pageSizeNew = Number(pageSize);
+                let pageNew = Number(page);
+                if (pageNew <= 1) pageNew = 1;
+                skipNumber = (pageNew - 1) * pageSizeNew;
+
+                AddressModel
+                    .find()
+                    .skip(skipNumber)
+                    .sort({ index: -1 })
+                    .limit(pageSize)
+                    .then((data: any) => {
+                        AddressModel
+                            .countDocuments(queryData)
+                            .then((total: number) => {
+                                var totalPage: number = Math.ceil(total / pageSizeNew);
+                                return res.status(200).json({
+                                    paganition: {
+                                        totalPage: Number(totalPage),
+                                        currentPage: Number(page),
+                                        pageSize: Number(pageSize),
+                                        totalElement: Number(total),
+                                    },
+                                    data,
+                                });
+                            })
+                            .catch((error: any) => next(error));
+                    });
+            } else {
+                // get all
+                AddressModel
+                    .find()
+                    .sort({ index: -1 })
+                    .then((data: any) => {
+                        return res.status(200).json(data);
+                    })
+                    .catch((err: any) => {
+                        return next(err);
+                    });
+            }
         } catch (error) {
             return res.status(400).json(error);
         }
