0f131d6e96168a1b3033889780119d6c806e71d8
Duc-tuan <phamductuan26032002@gmail.com>
2023-09-08 09:15:24 +0700

    update

--- dist/routers/router/AddressRouter.ts
+++ dist/routers/router/AddressRouter.ts
@@ -3,12 +3,12 @@ var router = express.Router();
 
 const AddressOrderContronller = require('../../controllers/AddressOrderContronller');
 
+// danh sách địa chỉ nhận hàng
+router.get('/detail/:id', AddressOrderContronller.getDetail);
 // thêm địa chỉ nhận hàng
 router.put('/', AddressOrderContronller.createAddressOrder);
 // chỉnh sửa địa chỉ nhận hàng
 router.patch('/:id', AddressOrderContronller.editAddressOrder);
-// danh sách địa chỉ nhận hàng
-router.get('/detail/:id', AddressOrderContronller.getDetail);
 // danh sách địa chỉ nhận hàng
 router.get('/', AddressOrderContronller.index);
 
