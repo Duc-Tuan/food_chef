ba8e289e3b5d9ef6d8ce92c34f8ccf98443a42aa
Duc-tuan <phamductuan26032002@gmail.com>
2023-09-10 21:38:26 +0700

    update

--- dist/controllers/UploadFileContrller.ts
+++ dist/controllers/UploadFileContrller.ts
@@ -1,6 +1,8 @@
-import { Response, Request } from 'express';
-import { deleteFile, uploadImages } from '../utils/firebase/funcFireBase';
+import { Request, Response } from 'express';
+import { getDownloadURL, ref, uploadString } from 'firebase/storage';
 import { nameFile } from '../styles';
+import { deleteFile, uploadImages } from '../utils/firebase/funcFireBase';
+import { giveCurrentDateTime, storage } from '../utils/firebase/upload-file.controller';
 class UploadFileController {
   //[GET] danh sách tài khoản tất cả hoặc theo phân trang page&pageSize hoặc tìm kiếm theo query&status
   async index(req: Request, res: Response, next: any) {
@@ -23,9 +25,22 @@ class UploadFileController {
     //   next(error);
     // }
     try {
-      console.log(req?.file);
       const dataFile = await uploadImages(req?.file, `images/${nameFile.productDetails}`);
       return res.status(200).json(dataFile);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async imageUpload(req: Request, res: Response, next: any) {
+    const randomName = giveCurrentDateTime();
+    try {
+      const { imageBase64, fileName = 'images/productDetails' } = req.body;
+      const storageRef = ref(storage, `${fileName}/${randomName}.png`);
+      uploadString(storageRef, `data:image/png;base64,${imageBase64}`, 'data_url').then(async (snapshot: any) => {
+        const downloadURL = await getDownloadURL(snapshot.ref);
+        return res.status(200).json({ downloadURL, nameFile: `${fileName}/${randomName}.png` });
+      });
     } catch (error) {
       next(error);
     }
--- dist/routers/router/UploadFileRouter.ts
+++ dist/routers/router/UploadFileRouter.ts
@@ -1,11 +1,11 @@
 import express from 'express';
-import { upload } from '../../utils/firebase/upload-file.controller';
 var router = express.Router();
 
 const uploadFileContrller = require('../../controllers/UploadFileContrller');
 
 // upload File
-router.post('/', upload.single('fileName'), uploadFileContrller.index);
+router.post('/', uploadFileContrller.index);
+router.post('/image', uploadFileContrller.imageUpload);
 router.delete('/', uploadFileContrller.detele);
 
 module.exports = router;
