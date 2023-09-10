c5b0d25b4cad24da91b0f44cb8563ffe7639492b
Duc-tuan <phamductuan26032002@gmail.com>
2023-09-10 21:58:25 +0700

    update

--- index.ts
+++ index.ts
@@ -29,8 +29,9 @@ app.use(function (req: Request, res: Response, next) {
   next();
 });
 
+app.use(express.json({ limit: '25mb' }));
+app.use(express.urlencoded({ extended: true, limit: '25mb' }));
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
 router(app);
 configUriImage(app);
