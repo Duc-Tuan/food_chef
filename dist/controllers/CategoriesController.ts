e1fffcfca96c8e9eb423f7257c2257368b359ffb
Duc-tuan <phamductuan26032002@gmail.com>
2023-09-28 09:27:14 +0700

    update

--- dist/controllers/CategoriesController.ts
+++ dist/controllers/CategoriesController.ts
@@ -9,15 +9,61 @@ class CategoriesController {
     //GET /categories
     async index(req: Request, res: Response, next: any) {
         try {
-            CategoriesModel
-                .find(undefined, { categoryImageMulter: 0 })
-                .sort({ index: -1 })
-                .then((data: any) => {
-                    return res.status(200).json(data);
-                })
-                .catch((err: any) => {
-                    return next(err);
-                });
+            var { page, pageSize, query } = req.query;
+            let dataSearch: any = undefined;
+            let queryData: any = undefined;
+
+            // query data search
+            if (query) {
+                dataSearch = { $regex: query, $options: 'i' };
+                queryData = {
+                    categoryName: dataSearch,
+                };
+            }
+
+            var skipNumber: number = 0;
+            // get page, pageSize, query and status data
+            if (page || pageSize) {
+                const pageSizeNew = Number(pageSize);
+                let pageNew = Number(page);
+                if (pageNew <= 1) pageNew = 1;
+                skipNumber = (pageNew - 1) * pageSizeNew;
+
+                CategoriesModel
+                    .find(queryData, { categoryImageMulter: 0, index: 0 })
+                    .skip(skipNumber)
+                    .sort({ index: -1 })
+                    .limit(pageSize)
+                    .then((data: any) => {
+                        CategoriesModel
+                            .countDocuments(queryData)
+                            .then((total: number) => {
+                                var totalPage: number = Math.ceil(total / pageSizeNew);
+                                return res.status(200).json({
+                                    paganition: {
+                                        totalPage: Number(totalPage),
+                                        currentPage: Number(page),
+                                        pageSize: Number(pageSize),
+                                        totalElement: Number(total),
+                                    },
+                                    data,
+                                });
+                            })
+                            .catch((error: any) => next(error));
+                    });
+            } else {
+                // get all
+                CategoriesModel
+                    .find(queryData, { categoryImageMulter: 0, index: 0 })
+                    .sort({ index: -1 })
+                    .then((data: any) => {
+                        return res.status(200).json(data);
+                    })
+                    .catch((err: any) => {
+                        return next(err);
+                    });
+            }
+
         } catch (error) {
             return res.status(400).json(error);
         }
